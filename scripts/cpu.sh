6f3c67100ccf4a69bb00e5f022078c2da5b45dc8
Alexey Samoshkin <al.samoshkin@gmail.com>
2017-12-12 18:40:10 +0200

    Split CPU collect logic (long-running) and CPU metric printing (quick) to make status bar refresh behavior more predictable

--- scripts/cpu.sh
+++ scripts/cpu.sh
@@ -8,12 +8,7 @@ LC_NUMERIC=C
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
-refresh_interval=$(get_tmux_option "status-interval" "5")
-
-# reduce interval by 1 second, so command has time to complete within tmux status refresh interval. Otherwise, tmux will use stale previous command result for 2 times in a row
-if [ "$refresh_interval" -gt "1" ]; then
-  refresh_interval="$(($refresh_interval - 1))"
-fi
+cpu_tmp_dir=$(tmux show-option -gqv "@sysstat_cpu_tmp_dir")
 
 cpu_view_tmpl=$(get_tmux_option "@sysstat_cpu_view_tmpl" 'CPU:#[fg=#{cpu.color}]#{cpu.pused}#[default]')
 
@@ -36,33 +31,8 @@ get_cpu_color(){
   fi
 }
 
-get_cpu_usage() {
-  if is_osx; then
-    if command_exists "iostat"; then
-      iostat -c 2 -w "$refresh_interval" | tail -n 1 | awk '{ print 100-$(NF-3) }'
-    else
-      top -l 2 -s "$refresh_interval" -n 0 | sed -nr '/CPU usage/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)%[[:space:]]*idle.*/\1/p' | tail -n 1 | awk '{ print 100-$0 }'
-    fi
-  else
-    if command_exists "vmstat"; then
-	if is_freebsd; then
-	    vmstat -n "$refresh_interval" -c 2 | tail -n 1 | awk '{print 100-$(NF-0)}'
-	else
-	    vmstat -n "$refresh_interval" 2 | tail -n 1 | awk '{print 100-$(NF-2)}'
-	fi
-    else
-	if is_freebsd; then
-	    top -d2 | sed -nr '/CPU:/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)%[[:space:]]*id.*/\1/p' | tail -n 1 | awk '{ print 100-$0 }'
-	else
-	    top -b -n 2 -d "$refresh_interval" | sed -nr '/%Cpu/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)[[:space:]]*id.*/\1/p' | tail -n 1 | awk '{ print 100-$0 }'
-	fi
-    fi
-  fi
-}
-
 print_cpu_usage() {
-  local cpu_pused=$(get_cpu_usage)
-
+  local cpu_pused=$(get_cpu_usage_or_collect)
   local cpu_color=$(get_cpu_color "$cpu_pused")
   
   local cpu_view="$cpu_view_tmpl"
@@ -72,6 +42,33 @@ print_cpu_usage() {
   cpu_view="${cpu_view//'#{cpu.color3}'/$(echo "$cpu_color" | awk '{ print $3 }')}"
 
   echo "$cpu_view"
+}
+
+get_cpu_usage_or_collect() {
+  local collect_cpu_metric="$cpu_tmp_dir/cpu_collect.metric"
+
+  # read cpu metric from file, otherwise 0 as a temporary null value, until first cpu metric is collected
+  [ -f "$collect_cpu_metric" ] && cat "$collect_cpu_metric" || echo "0.0"
+
+  start_cpu_collect_if_required >/dev/null 2>&1
+}
+
+start_cpu_collect_if_required() {
+  local collect_cpu_pidfile="$cpu_tmp_dir/cpu_collect.pid"
+
+  # check if cpu collect process is running, otherwise start it in background
+  if [ -f "$collect_cpu_pidfile" ] && ps -p "$(cat "$collect_cpu_pidfile")" > /dev/null 2>&1; then
+    return;
+  fi
+  
+  jobs >/dev/null 2>&1
+  "$CURRENT_DIR/cpu_collect.sh" &>/dev/null &
+  if [ -n "$(jobs -n)" ]; then
+    echo "$!" > "${collect_cpu_pidfile}"
+  else
+    echo "Failed to start CPU collect job" >&2
+    exit 1
+  fi
 }
 
 main(){
---
+++ scripts/cpu_collect.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+LC_NUMERIC=C
+
+set -u
+set -e
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$CURRENT_DIR/helpers.sh"
+
+refresh_interval=$(get_tmux_option "status-interval" "5")
+samples_count="60"
+cpu_metric_file="$(get_tmux_option "@sysstat_cpu_tmp_dir" "/dev/null")/cpu_collect.metric"
+
+get_cpu_usage() {
+  if is_osx; then
+    if command_exists "iostat"; then
+      iostat -w "$refresh_interval" -c "$samples_count" \
+        | stdbuf -o0 awk 'NR > 2 { print 100-$(NF-3); }'
+    else
+      top -l "$samples_count" -s "$refresh_interval" -n 0 \
+        | sed -u -nr '/CPU usage/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)%[[:space:]]*idle.*/\1/p' \
+        | stdbuf -o0 awk '{ print 100-$0 }'
+    fi
+  elif ! command_exists "vmstat"; then
+    if is_freebsd; then
+      vmstat -n "$refresh_interval" -c "$samples_count" \
+        | stdbuf -o0 awk 'NR>2 {print 100-$(NF-0)}'
+    else
+      vmstat -n "$refresh_interval" "$samples_count" \
+        | stdbuf -o0 awk 'NR>2 {print 100-$(NF-2)}'
+    fi
+  else
+    if is_freebsd; then
+      top -d"$samples_count" \
+        | sed -u -nr '/CPU:/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)%[[:space:]]*id.*/\1/p' \
+        | stdbuf -o0 awk '{ print 100-$0 }'
+    else
+      top -b -n "$samples_count" -d "$refresh_interval" \
+        | sed -u -nr '/%Cpu/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)[[:space:]]*id.*/\1/p' \
+        | stdbuf -o0 awk '{ print 100-$0 }'
+    fi
+  fi
+}
+
+main() {
+  get_cpu_usage | while read -r value; do
+    echo "$value" | tee "$cpu_metric_file"
+  done
+}
+
+main
+

7cffb4e370a10bda573011431f73861c6bcac08e
Willy Romão <willyr.goncalves@gmail.com>
2018-09-25 21:01:55 -0300

    fix compatibility with FreeBSD
    Fix compatibility with FreeBSD. I did test this changes in FreeBSD, MacOSX and Linux.

--- scripts/loadavg.sh
+++ scripts/loadavg.sh
@@ -7,17 +7,19 @@ LC_NUMERIC=C
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
+NULL="/dev/null"
 
 loadavg_per_cpu_core=$(get_tmux_option "@sysstat_loadavg_per_cpu_core" "true")
 
 get_num_of_cores(){
-  is_osx && sysctl -n hw.ncpu || nproc
+  is_osx && sysctl -n hw.ncpu && return 0
+  is_freebsd && sysctl -n hw.ncpu || nproc 2> ${NULL}
 }
 
-main(){ 
+main(){
   local num_cores=$([ "$loadavg_per_cpu_core" == "true" ]  && get_num_of_cores || echo 1)
 
-  uptime | awk -v num_cores="$num_cores" '{ 
+  uptime 2> ${NULL} | awk -v num_cores="$num_cores" '{
     sub(/,$/, "", $(NF-2));
     sub(/,$/, "", $(NF-1));
     sub(/,$/, "", $NF);
